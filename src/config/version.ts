f9da7739343d6f0d6ffaaf091d489dac44a4d176
Yokesh <Yokesh@fourtyfourty.in>
2025-05-20 20:50:56 +0530

    [minor] Test update

--- src/config/version.ts
+++ src/config/version.ts
@@ -2,7 +2,7 @@ export const VERSION = {
   major: 1,
   minor: 0,
   patch: 0,
-  build: process.env.GIT_COMMIT_SHA || 'dev',
+  build: 'dev',
   toString: function() {
     return `v${this.major}.${this.minor}.${this.patch}-${this.build}`;
   }
